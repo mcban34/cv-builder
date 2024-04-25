056fc37ba9395833ae1006ad19cbc5396e8e90b1
mehmet çoban <ccobanmehmet@gmail.com>
2024-04-26 00:43:41 +0300

    basic theme template

--- my-project/src/Components/Themes/Classic.jsx
+++ my-project/src/Components/Themes/Classic.jsx
@@ -5,38 +5,113 @@ import { Font, Document, Page, Text, View, StyleSheet, PDFViewer, PDFDownloadLin
 function Classic({ userData, cvThemeData }) {
   console.log("classic çalıştı", userData, cvThemeData)
 
+  // const styles = StyleSheet.create({
+  //   page: {
+  //     // flexDirection: 'row',
+  //     fontSize: 12,
+  //     padding: 5,
+  //     backgroundColor: '#E4E4E4',
+  //     fontFamily: 'Roboto'
+  //   },
+  //   section: {
+  //     // margin: 10,
+  //     // padding: 10,
+  //     // flexGrow: 1,
+  //   },
+  // });
+  // Font.register({
+  //   family: "Roboto",
+  //   src:
+  //     "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf"
+  // });
+
   const styles = StyleSheet.create({
     page: {
+      flexDirection: 'column',
+      backgroundColor: '#FFF',
+      padding: 30
+    },
+    personalInfo: {
       flexDirection: 'row',
-      backgroundColor: '#E4E4E4',
+      justifyContent: 'space-between',
+      marginBottom: 10
+    },
+    header: {
+      fontSize: 24,
+      fontFamily: 'Helvetica-Bold'
+    },
+    subHeader: {
+      fontSize: 18,
+      fontFamily: 'Helvetica'
+    },
+    text: {
+      fontSize: 12,
+      fontFamily: 'Helvetica'
     },
     section: {
-      margin: 10,
-      padding: 10,
-      flexGrow: 1,
-      fontFamily: 'Roboto'
-
+      marginVertical: 10,
     },
-  });
-  Font.register({
-    family: "Roboto",
-    src:
-      "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf"
+    sectionHeader: {
+      fontSize: 16,
+      fontFamily: 'Helvetica-Bold',
+      marginBottom: 5
+    },
+    listItem: {
+      flexDirection: 'row',
+      marginBottom: 5
+    },
+    bulletPoint: {
+      width: 10,
+      fontSize: 10,
+    },
+    listItemContent: {
+      flex: 1,
+    }
   });
 
-
-  const MyDocument = () => (
+  const MyDocument = ({ userData }) => (
     <Document>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.section}>
-          <Text>asıl amaç çoban rojin üğö</Text>
+      <Page style={styles.page}>
+        <View style={styles.personalInfo}>
+          <Text style={styles.header}>{`${userData.personalInfo.firstName} ${userData.personalInfo.lastName}`}</Text>
+          <Text style={styles.text}>{userData.personalInfo.phone} • {userData.personalInfo.email}</Text>
         </View>
+
         <View style={styles.section}>
-          <Text>Deneyimleriniz ve Yetenekleriniz</Text>
+          <Text style={styles.sectionHeader}>Summary</Text>
+          <Text style={styles.text}>{userData.summary}</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionHeader}>Experience</Text>
+          {userData.experience.map((item, index) => (
+            <View key={index} style={styles.listItem}>
+              <Text style={styles.bulletPoint}>•</Text>
+              <View style={styles.listItemContent}>
+                <Text style={styles.subHeader}>{item.position}, {item.companyName}</Text>
+                <Text style={styles.text}>{item.startDate} - {item.endDate}</Text>
+                <Text style={styles.text}>{item.description}</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionHeader}>Education</Text>
+          {userData.education.map((item, index) => (
+            <View key={index} style={styles.listItem}>
+              <Text style={styles.bulletPoint}>•</Text>
+              <View style={styles.listItemContent}>
+                <Text style={styles.subHeader}>{item.degree}, {item.fieldOfStudy}</Text>
+                <Text style={styles.text}>{item.schoolName}</Text>
+                <Text style={styles.text}>{item.startDate} - {item.endDate}</Text>
+              </View>
+            </View>
+          ))}
         </View>
       </Page>
     </Document>
-  );
+  )
 
   return (
     <div className='container mx-auto'>
@@ -55,19 +130,8 @@ function Classic({ userData, cvThemeData }) {
           </div>
         </div>
       </section>
-      {/* <PDFViewer>
-        <Document>
-          <Page size="A4" style={styles.page}>
-            <View style={styles.section}>
-              <Text>Section #1</Text>
-            </View>
-            <View style={styles.section}>
-              <Text>Section #2</Text>
-            </View>
-          </Page>
-        </Document>
-      </PDFViewer> */}
-      <PDFDownloadLink document={<MyDocument />} fileName="somename.pdf">
+
+      <PDFDownloadLink document={<MyDocument userData={userData} />} fileName={`${userData.personalInfo.firstName}_${userData.personalInfo.lastName}.pdf`}>
         {({ blob, url, loading, error }) =>
           loading ? 'Loading document...' : 'Download now!'
         }
